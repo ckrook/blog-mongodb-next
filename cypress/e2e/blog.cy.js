7f39856485a3436be3e47443da58d1e969bc110e
Charles Krook <charles.krook@gmail.com>
2023-01-18 21:45:58 +0100

    commit testing

--- cypress/e2e/blog.cy.js
+++ cypress/e2e/blog.cy.js
@@ -1,20 +1,3 @@
-// describe("Login", () => {
-//   it("should login through the Google auth flow", () => {
-//     // Start from the index page
-//     cy.visit("http://localhost:3000/");
-
-//     // Find a link with an href attribute containing "about" and click it
-//     cy.get("#signin").click();
-//     cy.get(".button").click();
-//     cy.origin("https://accounts.google.com", () => {
-//       cy.get("input[type=email]").type("charles.krook@gmail.com");
-//       cy.get(".VfPpkd-LgbsSe-OWXEXe-k8QpJ").click();
-//       cy.get("input[type=password]").type("Google.1234");
-//       cy.get(".VfPpkd-LgbsSe-OWXEXe-k8QpJ").click();
-//     });
-//   });
-// });
-
 describe("Read a Blogpost headline", () => {
   it("Should navigate to a blogpost and read the headline", () => {
     // Start from the index page
@@ -34,5 +17,18 @@ describe("Navigate blogpost and home", () => {
       firstMatchedDiv.click();
     });
     cy.get("#homelogolink").click();
+  });
+
+  describe("Copy blogpost url to clipboard", () => {
+    it("Should navigate into a blogpost and copy a url to clipboard", () => {
+      // Start from the index page
+      cy.visit("http://localhost:3000/");
+      cy.get("#header").then(($h1) => {
+        const firstMatchedDiv = $h1.find("h1").first();
+        firstMatchedDiv.click();
+      });
+      cy.get("button[data-id=copy-url-button]").click();
+      cy.assertValueCopiedToClipboard("http://localhost:3000/post/63c666611fde9604489568c8");
+    });
   });
 });
